c90d355c7b211af39deb1855aafeb703007775da
unknown <nguyendinhtuananh7@gmail.com>
2023-03-07 22:42:01 +0700

    first commit

---
+++ baithuchanh0303/src/Slide55.java
@@ -0,0 +1,11 @@
+public class Slide55 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("cac so chia het cho 5 tu 0 den 200 la:");
+        for (int i = 0; i <= 200; i++) {
+            if (i % 5 == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
+    
+
